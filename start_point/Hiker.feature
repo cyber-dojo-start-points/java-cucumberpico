39e4b071a1593799626609328fcfba41ed08d02b
Seb Rose <seb@claysnow.co.uk>
2020-06-23 19:48:11 +0100

    Updated to modern cucumber-jvm using Rule keyword

--- start_point/Hiker.feature
+++ start_point/Hiker.feature
@@ -1,7 +1,8 @@
 
 Feature: hitch-hiker playing scrabble
 
-Scenario: hitch-hiker playing scrabble in the past
-Given the hitch-hiker selects some tiles
-When they spell 6 times 9
-Then the score is 42
+Rule: the answer is always 42
+  Scenario: hitch-hiker playing scrabble in the past
+    Given the hitch-hiker selects some tiles
+    When they spell 6 times 9
+    Then the score is 42
--- start_point/HikerStepDef.java
+++ start_point/HikerStepDef.java
@@ -1,21 +1,22 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-import cucumber.api.java.en.*;
+import io.cucumber.java.en.*;
+
 
 public class HikerStepDef {
 
     private int answer;
 
-    @Given("^the hitch-hiker selects some tiles$")
+    @Given("the hitch-hiker selects some tiles")
     public void theHitchHikerSelectsSomeTiles() throws Throwable {
     }
 
-    @When("^they spell (\\d+) times (\\d+)$")
+    @When("they spell {int} times {int}")
     public void theySpellTimes(int arg1, int arg2) throws Throwable {
         answer = Hiker.answer(arg1,arg2);
     }
 
-    @Then("^the score is (\\d+)$")
+    @Then("the score is {int}")
     public void theScoreIs(int expected) throws Throwable {
         assertEquals(expected, answer);
     }
